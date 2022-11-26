5b4c50181ae3bcce2314897f151bec2540e9185f
malik188 <muhammad.malik01@ui.ac.id>
2022-11-26 17:39:05 +0700

    finalizing secure note popup

--- client/src/Components/Popup.js
+++ client/src/Components/Popup.js
@@ -4,11 +4,11 @@ function PopUp() {
   const [popup, setPop] = useState(false);
   const handleClickOpen = () => {
     setPop(!popup);
-    document.body.style.backgroundColor = "#D0CECE";
+    // document.body.style.backgroundColor = "#D0CECE";
   };
   const closePopup = () => {
     setPop(false);
-    document.body.style.backgroundColor = "#FFF5E9";
+    // document.body.style.backgroundColor = "#FFF5E9";
   };
   return (
     <div>
@@ -17,12 +17,23 @@ function PopUp() {
         {popup ? (
           <div className="mainPopup">
             <div className="popup">
-              <div className="popup-header">
-                <h1>popup</h1>
-                <h1 onClick={closePopup}>X</h1>
+              <div className="popup-header">Add Item
               </div>
-              <div>
-                <p>This is simple popup in React js</p>
+              <div className="popup-body">
+                <p>Type of Item</p>
+                <input list="type" name="type" placeholder="Secure Note" id="type_border"/>
+                <datalist id="type">
+                  <option value="Login" />
+                  <option value="Cards" />
+                  <option value="Secure Note" />
+                </datalist>
+                <p>Name</p>
+                <input type="text" id="noteName" /><br />
+                <p>Notes</p>
+                <textarea name="message" id="message"></textarea>
+                <br />
+                <button onClick={closePopup} id="saveButton">Save</button>
+                <button onClick={closePopup} id="cancelButton">Cancel</button>
               </div>
             </div>
           </div>
