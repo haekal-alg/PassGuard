b7736b41acd9fdddf7c810650a9e36396f3043dd
Alvitoikrm <alvitoikrm@gmail.com>
2022-12-12 23:52:53 +0700

    fix navbar route

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+function Navbar() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <img
+            src="./PG_logo.png"
+            classname="app-logo"
+            style={{ width: "150px", height: "45px" }}
+          ></img>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto"></ul>
+          </div>
+        </div>
+      </nav>
+      <div className="auth-wrapper">//taro sini</div>
+    </div>
+  );
+}
+
+export default Navbar;
